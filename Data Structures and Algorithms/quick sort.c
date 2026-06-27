46b3288f07042c13df242b4c316c5d42781fd99b
Aastha W <waastha4@gmail.com>
2026-06-27 23:33:37 +0400

    Add DSA material

---
+++ Data Structures and Algorithms/quick sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+// Function to partition array
+int partition(int arr[], int low, int high)
+{
+    // Choose last element as pivot
+    int pivot = arr[high];
+
+    int i = low - 1;
+
+    for(int j = low; j < high; j++)
+    {
+        // Place smaller elements before pivot
+        if(arr[j] < pivot)
+        {
+            i++;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // Place pivot in correct position
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+// Recursive Quick Sort Function
+void quickSort(int arr[], int low, int high)
+{
+    if(low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        // Sort left subarray
+        quickSort(arr, low, pi - 1);
+
+        // Sort right subarray
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted Array:\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
