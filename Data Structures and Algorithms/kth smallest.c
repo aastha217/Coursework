57aa28ff61e6d226463cf8214f3be553e0893298
Aastha W <waastha4@gmail.com>
2026-06-28 23:31:54 +0400

    Add DSA material

---
+++ Data Structures and Algorithms/kth smallest.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Function to sort array using Bubble Sort
+void sort(int arr[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {7,10,4,3,20,15};
+
+    int n = 6;
+    int k = 3;
+
+    sort(arr,n);
+
+    printf("%dth Smallest Element = %d",
+           k,
+           arr[k-1]);
+
+    return 0;
+}
