1a5ffeda443de19832404ad50db2900efdefab9f
Aastha W <waastha4@gmail.com>
2026-07-08 23:45:33 +0400

    add mysql material

--- Database information system/COVID Recovery Patients.sql
+++ Database information system/COVID Recovery Patients.sql
@@ -98,34 +98,10 @@ Output table is ordered by recovery_time in ascending order, and then by patient
 
 */
 
-WITH
-first_positive AS (
-    SELECT 
-        patient_id
-        ,MIN(test_date) AS first_positive_date
-    FROM covid_tests
-    WHERE result='Positive'
-    GROUP BY patient_id
-)
-,first_negative_after_positive AS(
-    SELECT 
-        fp.patient_id
-        ,MIN(ct.test_date) AS first_negative_date
-    FROM first_positive fp
-    JOIN covid_tests ct
-        ON fp.patient_id=ct.patient_id
-    WHERE ct.result='Negative' 
-        AND first_positive_date<ct.test_date
-    GROUP BY fp.patient_id
-)
-SELECT 
-    p.patient_id
-    ,p.patient_name
-    ,p.age
-    ,DATEDIFF(fn.first_negative_date ,fp.first_positive_date) AS recovery_time
-FROM first_positive fp
-JOIN first_negative_after_positive fn
-    ON fp.patient_id=fn.patient_id
-JOIN patients p
-    ON fp.patient_id=p.patient_id
-ORDER BY recovery_time ASC, p.patient_name ASC
+
+
+SELECT c.patient_id, p.patient_name, p.age, datediff(MIN(c1.test_date) , MIN(c.test_date)) recovery_time
+FROM covid_tests c INNER JOIN covid_tests c1 ON c.patient_id = c1.patient_id AND c.test_date < c1.test_date AND c.result = 'Positive' AND c1.result = 'Negative'
+INNER JOIN patients p ON c.patient_id = p.patient_id
+GROUP BY c.patient_id, p.patient_name, p.age
+ORDER BY recovery_time, p.patient_name;
